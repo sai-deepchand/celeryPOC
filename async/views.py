9a15798bec166fead7abf768b50b6118a5e7530c
sai deepchand <deepchandkunisetty.9@gmail.com>
2022-02-03 11:54:45 +0530

    minor changes

--- async/views.py
+++ async/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .tasks import sleepy
 
 def home(request):
-       sleepy.delay(15)
+       sleepy(5)
        return HttpResponse('<h1> welcome home !!!!!!! </h1> ')
 
        
