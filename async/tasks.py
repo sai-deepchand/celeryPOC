290d4fc6794fffc99c5f123b59c83b8b6e895d25
sai deepchand <deepchandkunisetty.9@gmail.com>
2022-02-07 13:29:29 +0530

    completed send mail in async branch

--- async/tasks.py
+++ async/tasks.py
@@ -1,12 +1,24 @@
 from celery import shared_task
 from time import sleep
-
-@shared_task
-def add(x, y):
-       return x + y
-
+from django.core.mail import send_mail
+from celeryPOC import settings
 
 @shared_task
 def sleepy(x):
        sleep(x)
        return None
+
+
+@shared_task
+def send_mail_func():
+       mail_subject = "testing celery"
+       message = "sending mail using celery one more time "
+       to_email = '2018129@iiitdmj.ac.in'
+       send_mail(
+              subject = mail_subject,
+              message=message,
+              from_email=settings.EMAIL_HOST_USER,
+              recipient_list=[to_email],
+              fail_silently=True,
+       )
+       return "Done"
